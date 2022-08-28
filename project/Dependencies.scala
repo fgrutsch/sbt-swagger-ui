9c097d04e7e8443ec91aa216db562e6e0c254305
Fabian Grutsch <f.grutsch@gmail.com>
2022-08-28 01:23:58 +0000

    Update jsoup to 1.15.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val plugin: Seq[ModuleID] = Seq(
-    "org.jsoup"      % "jsoup"     % "1.15.2",
+    "org.jsoup"      % "jsoup"     % "1.15.3",
     "org.scalatest" %% "scalatest" % "3.2.13" % Test
   )
 
