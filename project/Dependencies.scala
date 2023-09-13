9ae28cda1d4acdc32b27e89966a022a8c957dec1
Fabian Grutsch <f.grutsch@gmail.com>
2023-09-13 02:08:30 +0200

    Update scalatest to 3.2.17 (#81)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val plugin: Seq[ModuleID] = Seq(
     "org.jsoup"      % "jsoup"     % "1.16.1",
-    "org.scalatest" %% "scalatest" % "3.2.16" % Test
+    "org.scalatest" %% "scalatest" % "3.2.17" % Test
   )
 
   val organizeimports: ModuleID = "com.github.liancheng" %% "organize-imports" % "0.6.0"
